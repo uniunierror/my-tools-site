430be11c140cb87a529589ae1196de63f51a0402
uniunierror <username@users.noreply.github.com>
2025-12-14 03:03:51 +0900

    feat: Configure Next.js for fs access on Edge and standalone output

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    return config;
+  // Edge Runtimeでfsアクセスを許可するためにNext.jsのExperimental設定を使用します
+  // Next.js 14ではこの設定がServerless環境でfsモジュールの動作を改善します。
+  experimental: {
+    serverComponentsExternalPackages: ["fs"],
   },
+  
+  // OpenNextのために、Workersでコンテンツディレクトリをバンドルする設定を追加
+  // Next.js 14では 'output' の設定が必須
+  output: 'standalone', 
 };
 
 module.exports = nextConfig;
